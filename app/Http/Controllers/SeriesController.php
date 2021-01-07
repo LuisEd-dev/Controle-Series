8996e9b7ddb0f8667283ef8d6d2c749dd45f8c4d
unknown <luiseduardo123321@gmail.com>
2021-01-07 09:52:28 -0300

    armazenar series e ordenação

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,15 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Serie;
+use Illuminate\Http\Request;
+
 class SeriesController extends Controller
 {
     public function index(){
-        $series = ["Grey's Anatomy", "Lost", "Agents of SHIELD"];
-
+        $series = Serie::query()->orderBy("nome")->get();
         return view('series.index', compact('series'));
     }
 
     public function create(){
         return view('series.create');
+    }
+
+    public function store(Request $request){
+        Serie::create([ 'nome' => $request->nome ]);
+
+        return redirect('/series');
+
     }
 }
